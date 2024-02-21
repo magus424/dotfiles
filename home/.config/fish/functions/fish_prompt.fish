a5d9341ce35108d83158492226e80ee8da5aebe9
Collin Grady <collin@collingrady.com>
2024-02-21 14:03:36 -0800

    updated git prompt again

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,10 +1,4 @@
 function __my_git_prompt
-    set icon_branch \ue0a0          # 
-    set icon_right_full \ue0b0      # 
-    set icon_right_empty \ue0b1     # 
-    set icon_left_full \ue0b2       # 
-    set icon_left_empty \ue0b3      # 
-
     git config core.bare &> /dev/null
     set -l git_status $status
 
@@ -12,17 +6,36 @@ function __my_git_prompt
         set git_branch (git rev-parse --abbrev-ref HEAD)
         set git_description (git config branch.$git_branch.description)
 
-        set_color A8A8A8
-        echo -n "$icon_branch "
-        set_color normal
-        echo -n (__fish_git_prompt '%s')
-        if test -n "$git_description"
-            set_color 626262
-            echo -n " $icon_right_empty "
-            set_color AF87D7
-            echo $git_description
-            set_color normal
+        set git_desc_prompt (__my_git_desc_prompt)
+        set git_desc_length (string length -V "$git_desc_prompt")
+
+        __my_git_branch_prompt
+        if test -n "$git_desc_prompt" -a $COLUMNS -gt (math $git_desc_length + 20)
+            __my_git_desc_prompt
         end
+    end
+end
+
+function __my_git_branch_prompt
+    set icon_branch \ue0a0          # 
+
+    set_color A8A8A8
+    echo -n "$icon_branch "
+    set_color normal
+    echo -n (__fish_git_prompt '%s')
+end
+
+function __my_git_desc_prompt
+    set icon_right_empty \ue0b1     # 
+    set git_branch (git rev-parse --abbrev-ref HEAD)
+    set git_description (git config branch.$git_branch.description)
+
+    if test -n "$git_description"
+        set_color 626262
+        echo -n " $icon_right_empty "
+        set_color AF87D7
+        echo $git_description
+        set_color normal
     end
 end
 
@@ -33,19 +46,17 @@ function fish_prompt --description 'Write out the prompt'
         fixssh
     end
 
-    set icon_sqrt \u221a    # √
+    set icon_sqrt \u221a            # √
+
+    set main_prompt (set_color 0BB)(hostname -s)(set_color 555):(set_color 55F)(prompt_pwd_long)(set_color normal)
+    set main_length (string length -V $main_prompt)
+    if test $COLUMNS -lt (math $main_length + 5)
+        set main_prompt (set_color 0BB)(hostname -s)(set_color 555):(set_color 55F)(prompt_pwd)(set_color normal)
+        set main_length (string length -V $main_prompt)
+    end
 
     set git_prompt (__my_git_prompt)
-    if test $COLUMNS -lt 50
-        set prompt_pwd_dynamic (prompt_pwd)
-    else
-        set prompt_pwd_dynamic (prompt_pwd_long)
-    end
-    set main_prompt (set_color 0BB)(hostname -s)(set_color 555):(set_color 55F)$prompt_pwd_dynamic(set_color normal)
-
     set git_length (echo (string length -V $git_prompt) | cut -d' ' -f1)
-    # set git_length (echo $git_length | cut -d' ' -f1)
-    set main_length (string length -V $main_prompt)
 
     set prompt_length (math $main_length + $git_length + 10)
 
