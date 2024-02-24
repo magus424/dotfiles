6f019cb8229cc26578d1b634a8d42f16b91a35b3
Collin Grady <collin@collingrady.com>
2024-02-24 15:23:59 -0800

    more prompt updates

--- home/.config/fish/conf.d/git_prompt_vars.fish
+++ home/.config/fish/conf.d/git_prompt_vars.fish
@@ -2,7 +2,10 @@
 set __fish_git_prompt_show_informative_status 1
 set __fish_git_prompt_use_informative_chars 1
 
-# set __fish_git_prompt_color ??
+set git_bg 002000
+
+# set __fish_git_prompt_color normal -b $git_bg
+# set __fish_git_prompt_color_done -b $git_bg
 # set __fish_git_prompt_color_prefix ??
 # set __fish_git_prompt_color_suffix ??
 # set __fish_git_prompt_color_bare ??
@@ -10,15 +13,15 @@ set __fish_git_prompt_use_informative_chars 1
 set __fish_git_prompt_char_cleanstate '' # '✔'
 # set __fish_git_prompt_color_cleanstate ??
 
-set __fish_git_prompt_char_stateseparator ' '
+set __fish_git_prompt_char_stateseparator ''
 
 set __fish_git_prompt_showdirtystate 1
-set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_dirtystate \ua0\uf4d2\ua0 # '*'
 set __fish_git_prompt_char_invalidstate '#'
-set __fish_git_prompt_char_stagedstate '+'
+set __fish_git_prompt_char_stagedstate \ua0\uf4d0\ua0 # '+'
 
 set __fish_git_prompt_showuntrackedfiles 1
-set __fish_git_prompt_char_untrackedfiles '%'
+set __fish_git_prompt_char_untrackedfiles \ua0\uf4a5\ua0 # '%'
 
 # auto, verbose, name, informative
 set __fish_git_prompt_showupstream informative
@@ -30,12 +33,15 @@ set __fish_git_prompt_char_upstream_prefix ' ' # μυ
 # set __fish_git_prompt_char_upstream_equal '='
 
 set __fish_git_prompt_showcolorhints 1
-set __fish_git_prompt_color_upstream 0087FF
-set __fish_git_prompt_color_dirtystate AF0000           # (red with showcolorhints, same as color_flags otherwise)
+# set __fish_git_prompt_color_upstream 0087FF
+set __fish_git_prompt_color_upstream 0087ff -b $git_bg
+set __fish_git_prompt_color_upstream_done -b $git_bg
+set __fish_git_prompt_color_dirtystate af0000 -b $git_bg
 # set __fish_git_prompt_color_invalidstate ??
-set __fish_git_prompt_color_stagedstate 00AF00          # (green with showcolorhints, color_flags otherwise)
-set __fish_git_prompt_color_untrackedfiles AF5F00       # (same as color_flags)
-set __fish_git_prompt_color_branch 00AF00               # (green) - the color of the branch if nothing else applies
+set __fish_git_prompt_color_stagedstate 00af00 -b $git_bg
+set __fish_git_prompt_color_untrackedfiles af5f00 -b $git_bg
+set __fish_git_prompt_color_branch 00af00 -b $git_bg
+set __fish_git_prompt_color_branch_done -b $git_bg
 set __fish_git_prompt_color_branch_detached D70000      # (red) the color of the branch if it’s detached (e.g. a commit is checked out)
 # set __fish_git_prompt_color_branch_dirty ??           # (no default) the color of the branch if it’s dirty and not detached
 # set __fish_git_prompt_color_branch_staged ??          # (no default) the color of the branch if it just has something staged and is otherwise clean
--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -18,37 +18,65 @@ end
 
 function __my_git_branch_prompt
     set icon_branch \ue0a0          # 
+    set icon_half_left \ue0b6       # 
+    set icon_half_right \ue0b4      # 
+    source ~/.config/fish/conf.d/git_prompt_vars.fish
 
-    set_color A8A8A8
+    set_color $git_bg
+    echo -n $icon_half_left
+    set_color 00a0a0 -b $git_bg
     echo -n "$icon_branch "
     set_color normal
     echo -n (__fish_git_prompt '%s')
+    set_color $git_bg
+    echo -n $icon_half_right
 end
 
 function __my_git_desc_prompt
     set icon_right_empty \ue0b1     # 
+    set icon_half_left \ue0b6       # 
+    set icon_half_right \ue0b4      # 
+    set icon_file_text \uf0f6       # 
+    set icon_file \uf4a5            # 
+    set nbsp \ua0
+
     set git_branch (git rev-parse --abbrev-ref HEAD)
     set git_description (git config branch.$git_branch.description)
 
+    set desc_bg 201030
     if test -n "$git_description"
-        set_color 626262
-        echo -n " $icon_right_empty "
-        set_color AF87D7
-        echo $git_description
+        echo -n " "
+        set_color $desc_bg
+        echo -n $icon_half_left
+        set_color AF87D7 -b $desc_bg
+        echo -n "$icon_file_text$nbsp$git_description"
+        set_color $desc_bg -b normal
+        echo -n $icon_half_right
         set_color normal
+        echo
     end
 end
 
 function fish_prompt --description 'Write out the prompt'
     echo
 
+    # if test $fish_term24bit -eq 0
+    #     echo "24bit unset"
+    # end
+
     if test -n "$TMUX"
         fixssh
     end
 
     set icon_sqrt \u221a            # √
+    set icon_folder \uf07b          # 
+    set icon_half_left \ue0b6       # 
+    set icon_half_right \ue0b4      # 
 
-    set main_prompt (set_color 0BB)(hostname -s)(set_color 555):(set_color 55F)(prompt_pwd_long)(set_color normal)
+    set pwd_fg 5f5fff
+    set pwd_bg 101030
+
+    set main_prompt (set_color $pwd_bg)$icon_half_left(set_color -b $pwd_bg $pwd_fg)$icon_folder\ua0(prompt_pwd_long)(set_color -b normal $pwd_bg)$icon_half_right
     set main_length (string length -V $main_prompt)
     if test $COLUMNS -lt (math $main_length + 5)
         set main_prompt (set_color 0BB)(hostname -s)(set_color 555):(set_color 55F)(prompt_pwd)(set_color normal)
