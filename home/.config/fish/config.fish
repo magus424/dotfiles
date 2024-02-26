b0918a15f5d2e91ecd393eb3425b112c9649c54a
Collin Grady <collin@collingrady.com>
2024-02-25 20:18:29 -0800

    add homebrew to config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -63,6 +63,10 @@ for p in $HOME/bin $HOME/.local/bin
     end
 end
 
+if test -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
 complete -f -c git -n '__fish_git_needs_command' -a get -d 'Checkout and switch to a branch'
 complete -f -c git -n '__fish_git_using_command get' -a '(__fish_git_branches)' --description 'Branch'
 complete -f -c git -n '__fish_git_needs_command' -a delbr -d 'Delete a branch from both local and remote repos'
