71ee7876e425ebefb19ab24423ea7f9760362cbb
Collin Grady <collin@collingrady.com>
2024-04-17 15:06:00 -0700

    add a note about session times

--- home/.config/fish/functions/__fish_aws_sso_login_status.fish
+++ home/.config/fish/functions/__fish_aws_sso_login_status.fish
@@ -3,14 +3,19 @@ function __fish_aws_sso_login_status --description 'check aws sso login status'
         for json in ~/.aws/sso/cache/*.json
             set -l startUrl (jq -cr '.startUrl' $json)
             if test $startUrl != "null"
+                # this date will only ever be ~1hr in the future running any aws
+                # command will update the date if the sso session is still valid
+                #
+                # The following user cron entry would do to keep it updated:
+                #
+                # 0 8-20 * * MON-FRI aws sts get-caller-identity --query Account > /dev/null 2>&1
+
                 set -l expiresAtISO (jq -cr '.expiresAt' $json)
                 set -l nowISO (date +"%Y-%m-%dT%H:%M:%S%z")
-                # echo "exp ISO: $expiresAtISO"
-                # echo "now ISO: $nowISO"
+
                 set -l expiresAt (date -d "$expiresAtISO" +"%s")
                 set -l now (date +"%s")
-                # echo "exp: $expiresAt"
-                # echo "now: $now"
+
                 if test $expiresAt -gt $now
                     echo "active"
                 else
