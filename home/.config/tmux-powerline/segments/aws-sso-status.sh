0d324a78b4dd0e983e6fdd270c63d8b9e10dc5ba
Collin Grady <collin@collingrady.com>
2024-04-26 16:49:59 -0700

    add aws status tmux segment

---
+++ home/.config/tmux-powerline/segments/aws-sso-status.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+run_segment() {
+    cloud="\uf0c2"
+
+    if ! test -f /tmp/tmux-aws-status.txt; then
+        touch /tmp/tmux-aws-status.txt
+    fi
+
+    if test `find /tmp/tmux-aws-status.txt -mmin +5`; then
+        aws sts get-caller-identity --query Account --output text 2> /dev/null
+        if [[ $? -eq 0 ]]; then
+            echo "active" > /tmp/tmux-aws-status.txt
+        else
+            echo "inactive" > /tmp/tmux-aws-status.txt
+        fi
+    fi
+
+    status=`cat /tmp/tmux-aws-status.txt`
+
+    if [[ "$status" == "active" ]]; then
+        echo -e "#[fg=#008000]${cloud}"
+    else
+        echo -e "#[fg=#800000]${cloud}"
+    fi
+
+    return 0
+}
--- home/.config/tmux-powerline/themes/default.sh
+++ home/.config/tmux-powerline/themes/default.sh
@@ -2,18 +2,18 @@
 # If changes made here does not take effect, then try to re-create the tmux session to force reload.
 
 if patched_font_in_use; then
-	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
-	TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
-	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
-	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
+    TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
+    TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
+    TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
+    TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
 else
-	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
-	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
+    TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
+    TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
+    TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
+    TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
 fi
 
-TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'235'}
+TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'234'}
 TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'255'}
 
 TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}
@@ -73,35 +73,36 @@ TMUX_POWERLINE_WINDOW_STATUS_STYLE="#[fg=colour248,nobold]"
 # of options to support the spacing_disable and separator_disable features can be used
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
-	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
-		# "hostname 33 0" \
-		#"ifstat 30 255" \
-		#"ifstat_sys 30 255" \
-		# "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		# "wan_ip 24 255" \
-	)
+    TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
+        "tmux_session_info 148 234" \
+        "aws-sso-status #232f3e 0" \
+        # "hostname 33 0" \
+        #"ifstat 30 255" \
+        #"ifstat_sys 30 255" \
+        # "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+        # "wan_ip 24 255" \
+    )
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
-	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		# "vcs_branch 29 88" \
-		# "vcs_compare 60 255" \
-		# "vcs_staged 64 255" \
-		# "vcs_modified 9 255" \
-		# "vcs_others 245 0" \
-		# "earthquake 3 0" \
-		# "pwd 89 211" \
-		# "now_playing 234 37" \
-		# "cpu 240 136" \
-		# "load 237 167" \
-		# "tmux_mem_cpu_load 234 136" \
-		# "weather 37 255"
-		# "rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
-		# "xkb_layout 125 117" \
-		# "date_day 235 136" \
-		# "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		# "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
-		# "utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
-	)
+    TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+        # "vcs_branch 29 88" \
+        # "vcs_compare 60 255" \
+        # "vcs_staged 64 255" \
+        # "vcs_modified 9 255" \
+        # "vcs_others 245 0" \
+        # "earthquake 3 0" \
+        # "pwd 89 211" \
+        # "now_playing 234 37" \
+        # "cpu 240 136" \
+        # "load 237 167" \
+        # "tmux_mem_cpu_load 234 136" \
+        # "weather 37 255"
+        # "rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+        # "xkb_layout 125 117" \
+        # "date_day 235 136" \
+        # "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+        # "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+        # "utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+    )
 fi
