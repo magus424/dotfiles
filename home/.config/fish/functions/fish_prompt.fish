62d1e200fc9973448708f1a9de3b76958215862d
Collin Grady <collin@collingrady.com>
2024-05-01 15:15:51 -0700

    clean up old prompt segment

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -76,15 +76,7 @@ function fish_prompt --description 'Write out the prompt'
     set pwd_fg 5f5fff
     set pwd_bg 101030
 
-    set aws_prompt ''
-    # set aws_status (__fish_aws_sso_login_status)
-    # if test "active" = "$aws_status"
-    #     set aws_prompt (set_color 080)"$icon_cloud "(set_color normal)
-    # else if test "expired" = "$aws_status"
-    #     set aws_prompt (set_color 800)"$icon_cloud "(set_color normal)
-    # end
-
-    set main_prompt $aws_prompt(set_color $pwd_bg)$icon_half_left(set_color -b $pwd_bg $pwd_fg)$icon_folder\ua0(prompt_pwd_long)(set_color -b normal $pwd_bg)$icon_half_right
+    set main_prompt (set_color $pwd_bg)$icon_half_left(set_color -b $pwd_bg $pwd_fg)$icon_folder\ua0(prompt_pwd_long)(set_color -b normal $pwd_bg)$icon_half_right
     set main_length (string length -V $main_prompt)
     if test $COLUMNS -lt (math $main_length + 5)
         set main_prompt (set_color 0BB)(hostname -s)(set_color 555):(set_color 55F)(prompt_pwd)(set_color normal)
