400bde2bfc3dd05f270b92df1f51776877c95f4f
Collin Grady <collin@collingrady.com>
2024-05-01 15:12:54 -0700

    improve tmux aws segment

--- home/.config/tmux-powerline/segments/aws-sso-status.sh
+++ home/.config/tmux-powerline/segments/aws-sso-status.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+update_aws_status() {
+    aws sts get-caller-identity --query Account --output text 2> /dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "active" > /tmp/tmux-aws-status.txt
+    else
+        echo "inactive" > /tmp/tmux-aws-status.txt
+    fi
+}
+
 run_segment() {
     cloud="\uf0c2"
 
     if ! test -f /tmp/tmux-aws-status.txt || test `find /tmp/tmux-aws-status.txt -mmin +5`; then
-        aws sts get-caller-identity --query Account --output text 2> /dev/null
-        if [[ $? -eq 0 ]]; then
-            echo "active" > /tmp/tmux-aws-status.txt
-        else
-            echo "inactive" > /tmp/tmux-aws-status.txt
-        fi
+        update_aws_status 2>&1 > /dev/null &
     fi
 
     status=`cat /tmp/tmux-aws-status.txt`
