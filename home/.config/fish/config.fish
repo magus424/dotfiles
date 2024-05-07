3fba11fef46996382efd54843e688c4fecd136ad
Collin Grady <collin@collingrady.com>
2024-05-07 14:27:34 -0700

    ssh agent updates

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,7 @@
-__fish_ssh_start_agent
-__fish_ssh_add_all_keys
+if test -z "$TMUX"
+    fish_ssh_agent
+    __fish_ssh_add_all_keys
+end
 
 if test -d /home/linuxbrew/.linuxbrew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
@@ -25,8 +27,6 @@ set -e GREP_OPTIONS
 set -x ACK_COLOR_MATCH "bold yellow"
 set -x EDITOR vim
 set -x GREP_COLOR "1;33"
-set -x LANG en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
 set -x NPM_TOKEN (gh auth token)
 set -x POWERLINE_COMMAND ~/.config/vim/bundle/powerline/scripts/powerline-render
 set -x RIPGREP_CONFIG_PATH ~/.ripgreprc
