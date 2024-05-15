61deef8c3f0a11d2dd14ee32508bf15d7dee279c
Collin Grady <collin@collingrady.com>
2024-05-15 16:11:39 -0700

    nvim/tmux updates

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -18,5 +18,20 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "alexghergh/nvim-tmux-navigation" },
+    {
+        'alexghergh/nvim-tmux-navigation',
+        config = function()
+            require'nvim-tmux-navigation'.setup {
+                disable_when_zoomed = true, -- defaults to false
+                keybindings = {
+                    left = "<C-h>",
+                    down = "<C-j>",
+                    up = "<C-k>",
+                    right = "<C-l>",
+                    last_active = "<C-\\>",
+                    next = "<C-Space>",
+                }
+            }
+        end
+    }
 })
