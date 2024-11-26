09aca48ef346d0387c37b62a011e034bddb1a607
Collin Grady <collin@collingrady.com>
2024-11-26 14:55:24 -0800

    fix: correct problem in kf command

--- home/.config/fish/functions/kf.fish
+++ home/.config/fish/functions/kf.fish
@@ -1,14 +1,12 @@
 function __kf_context
     kubectl config get-contexts -o name | \
         fzf-tmux -p 85,30 \
-            --header-lines=1 \
-            --bind 'enter:become(kubectl config use-context {2})'
+            --bind 'enter:become(kubectl config use-context {1})'
 end
 
 function __kf_namespace
     kubectl get namespaces | \
         fzf-tmux -p 50,30 \
-            --header-lines=1 \
             --bind 'enter:become(kubectl config set-context --current --namespace={1})'
 end
 
