5bcf51076d189dc56728e0bd04f78e35d6e2cf5b
Collin Grady <collin@collingrady.com>
2025-10-20 14:19:47 -0700

    add vscode keybind

--- home/.config/fish/functions/fish_user_key_bindings.fish
+++ home/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,6 @@
-bind \e\[1~ beginning-of-line
-bind \e\[3~ delete-char
-bind \e\[4~ end-of-line
+function fish_user_key_bindings
+  # bind \e\[1~ beginning-of-line
+  # bind \e\[3~ delete-char
+  # bind \e\[4~ end-of-line
+  bind \ev vscode_fzf
+end
