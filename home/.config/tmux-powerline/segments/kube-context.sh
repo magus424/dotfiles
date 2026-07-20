981642122ba0a70019f7ff670f2b3e712672035f
Collin Grady <collin@collingrady.com>
2026-07-20 13:32:15 -0700

    fix: stop using kubectl to read current context

--- home/.config/tmux-powerline/segments/kube-context.sh
+++ home/.config/tmux-powerline/segments/kube-context.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 run_segment() {
-    context=`kubectl config current-context`
+    context=`yq -o=json --unwrapScalar .current-context ~/.kube/config`
+    current_ns=`yq -o=json --unwrapScalar ".contexts[] | select(.name == \"${context}\") | .context.namespace" ~/.kube/config`
     context_trimmed=`echo "$context" | sed -e 's@arn.*cluster/@@'`
-    current_ns=`kubectl config view -o "jsonpath={.contexts[?(@.name==\"${context}\")].context.namespace}"`
 
     echo -n "$context_trimmed"
     if [[ -n "$current_ns" ]]; then
